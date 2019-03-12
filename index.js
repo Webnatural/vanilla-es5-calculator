e4efb5db650122d416227fe96f1bb4abb5f3e417
Dariusz Zielonka <info@dariusz-zielonka.pl>
2019-03-12 05:48:49 +0100

    Style and JS improvements, addedd local save to CSV

--- index.js
+++ index.js
@@ -1,211 +1,309 @@
+var WidgetCalculator = function () {
+    var calculatorform = document.querySelector('.calculator');
+    var controls = document.querySelector('.control-buttons');
+    var preview = document.getElementById("former-equation");
+    var output = document.querySelector('.current-calculation-display');
 
-var calculatorform = document.querySelector('.calculator');
-var controls = document.querySelector('.controls');
-var output = document.querySelector('.current-calculation-display');
-var digits = document.querySelectorAll('.digit');
-var operators = document.querySelectorAll('.operator');
-var equals = document.querySelector('.equals');
-var options = document.querySelectorAll('.option');
-
-
-//Model
-var model = {
-    tempResult: 0,
-    wholeEquation: [],
-    firstOperand: 0,
-    secondOperand: 0,
-    currentInput: 0,
-    operator: '',
-    executed: false,
-    addition() {
-        model.tempResult = parseFloat(model.firstOperand) + parseFloat(model.secondOperand);
-    },
-    subtraction() {
-        model.tempResult = parseFloat(model.firstOperand) - parseFloat(model.secondOperand);
-    },
-    multiplication() {
-        model.tempResult = parseFloat(model.firstOperand) * parseFloat(model.secondOperand);
-    },
-    division() {
-        model.tempResult = parseFloat(model.firstOperand) / parseFloat(model.secondOperand);
-    },
-    updateWholeEquation() {
-        model.wholeEquation.push([model.firstOperand, model.operator, model.secondOperand, model.tempResult]);
-        /*if ( Math.round(model.tempResult) == model.tempResult ) {
-            model.decimals = false;
-        }*/
-        //console.log(model.wholeEquation);
-        //console.log(model.firstOperand,model.operator,model.secondOperand,model.tempResult);
-
-        document.getElementById("former-equation").value = model.firstOperand + model.operator + model.secondOperand + "=" + model.tempResult
-        //console.log(model.currentInput)
-    }
-
-}
-
-var view = {
-    updateDisplay() {
-        output.textContent = model.currentInput;
-    },
-    showResult() {
-        if (model.tempResult.length > 10 && model.tempResult.includes('.')) {
-            output.textContent = parseFloat(model.tempResult.toFixed(4));
-        } else {
-            output.textContent = model.tempResult;
-        }
-    }
-}
-
-var controller = {
-    setUpHandlers() {
-
-        document.addEventListener('keypress', controller.handleKeyboardInput);
-        output.textContent = "0";
-
-    },
-
-    handleKeyboardInput(event) {
-
-        let key = event.key;
-        if (parseInt(key) >= 0 && parseInt(key) <= 9) {
-            console.log(parseInt(key) >= 0 && parseInt(key) <= 9)
-            controller.triggerDigit(key)
+    //Model
+    var model = {
+        result: 0,
+        wholeEquation: [],
+        firstOperand: 0,
+        secondOperand: 0,
+        lastInput: 0,
+        currentInput: '',
+        operator: '',
+        performedCalculation: false,
+        addition: function () {
+            model.result = parseFloat(model.firstOperand) + parseFloat(model.secondOperand);
+        },
+        subtraction: function () {
+            model.result = parseFloat(model.firstOperand) - parseFloat(model.secondOperand);
+        },
+        multiplication: function () {
+            model.result = parseFloat(model.firstOperand) * parseFloat(model.secondOperand);
+        },
+        division: function () {
+            model.result = parseFloat(model.firstOperand) / parseFloat(model.secondOperand);
         }
 
-        let operatorsChars = [
+    }
+
+    var view = {
+        updateDisplay: function (reset) {
+            if (reset) {
+                output.textContent = '0';
+                preview.value = "";
+            } else {
+                output.textContent = model.currentInput;
+            }
+        },
+        showResult: function () {
+            document.getElementById("former-equation").value = model.firstOperand + model.operator + model.secondOperand + "=" + model.result;
+            if (model.result.length > 10 && model.result.indexOf('.') !== -1) {
+                output.textContent = parseFloat(model.result.toFixed(4));
+            } else {
+                output.textContent = model.result;
+            }
+        }
+    }
+
+    var controller = {
+
+
+        operatorsKeys: [
             '+',
             '-',
             '*',
             '/'
-        ];
+        ],
 
-        // switch may be slow in comparison to if/else, so we use faster method
-        if (operatorsChars.includes(key)) {
-            controller.triggerOperator(key)
-        } else if (key == "." || key == ",") {
-            controller.triggerOption(".")
-        }
+        setUpHandlers: function () {
 
-        // however we have very limited set, so let's play with switch :)
-        switch (key) {
-            case 'c':
-                controller.triggerOption("AC")
-                break;
-            case '=':
-                controller.execute();
-                break;
-        }
 
-    },
-    handleClickDupe(event) {
-        event.preventDefault(); //prevent default behavior
-        if (event.type == "touchstart") {
-            // Handle touchstart event.
-        } else if (event.type == "click") {
-            // Handle click event.
-        }
-    },
-    triggerDigit(digit) {
-        if (model.currentInput) {
-            if (model.executed == false) {
+
+            document.addEventListener('keypress', controller.handleKeyboardInput);
+            calculatorform.addEventListener('input', function (event) {
+                console.log(this.event + "!!!");
+            });
+            controls.addEventListener('click', controller.handlePointerInput);
+            //controls.addEventListener('touchend', controller.handlePointerInput);
+
+        },
+        updateWholeEquation: function (reset) {
+            if (!reset) {
+                model.wholeEquation.push([model.firstOperand.toString() + model.operator.toString() + model.secondOperand.toString() + "=" + model.result]);
+            } else {
+                model.wholeEquation = [];
+            }
+        },
+        handlePointerInput: function (event) {
+            if (event.type == "touchend") {
+                //event.preventDefault()
+                //document.activeElement.blur();
+            } else if (event.type == "click") {
+
+            }
+            var key = event.target.value;
+            if (parseInt(key) >= 0 && parseInt(key) <= 9) {
+                controller.triggerDigit(key)
+                navigator.vibrate([50]);
+            }
+
+            // switch may be slow in comparison to if/else, so we use faster method
+            if (controller.operatorsKeys.indexOf(key) !== -1) {
+                controller.triggerOperator(key)
+                navigator.vibrate([50]);
+            } else if (key == ".") {
+                controller.triggerOption(".")
+                navigator.vibrate([50]);
+            }
+
+            // however we have very limited set, so let's play with switch :)
+            switch (key) {
+                case 'AC':
+                    controller.triggerOption("AC")
+                    navigator.vibrate([50]);
+                    break;
+                case 'Save':
+                    controller.save();
+                    navigator.vibrate([50]);
+                    break;
+                case '=':
+                    controller.performCalculation();
+                    navigator.vibrate([50]);
+                    break;
+            }
+        },
+        handleKeyboardInput: function (event) {
+
+            var key = event.key;
+            if (parseInt(key) >= 0 && parseInt(key) <= 9) {
+                controller.triggerDigit(key)
+            }
+
+            // switch may be slow in comparison to if/else, so we use faster method
+
+            if (controller.operatorsKeys.indexOf(key) !== -1) {
+                controller.triggerOperator(key)
+            } else if (key == "." || key == ",") {
+                controller.triggerOption(".")
+            }
+
+            // however we have very limited set, so let's play with switch :)
+            switch (key) {
+                case 'c':
+                    controller.triggerOption("AC")
+                    break;
+                case 's':
+                    controller.save();
+                    break;
+                case '=':
+                    controller.performCalculation();
+                    break;
+            }
+
+        },
+        handleClickDupe: function (event) {
+            event.preventDefault(); //prevent default behavior
+            if (event.type == "touchstart") {
+                // Handle touchstart event.
+            } else if (event.type == "click") {
+                // Handle click event.
+            }
+        },
+        triggerDigit: function (digit) {
+            if (model.currentInput !== "" || model.currentInput === "0.") {
+                //if (model.performedCalculation == false) {
                 model.currentInput += digit;
                 view.updateDisplay();
-            }
-            model.executed = false;
-        }
-        else {
-            if (digit !== '0') {
-                model.currentInput += digit;
-                view.updateDisplay();
-            }
-        }
-    },
-    triggerOperator(operator) {
-        if (!model.executed) {
-            controller.execute();
-        }
-        model.executed = false;
-        model.operator = operator;
-        model.currentInput = output.textContent;
-        model.firstOperand = model.currentInput;
-        model.currentInput = '';
-        console.log(model)
-    },
-    execute() {
-        console.log(model)
-        if (model.currentInput == "") { model.currentInput = model.secondOperand }
-        switch (model.operator) {
-            case '+':
-                model.secondOperand = model.currentInput;
-                model.addition();
-                model.updateWholeEquation();
-                view.showResult();
-                model.firstOperand = model.tempResult;
-                break;
-            case '-':
-                model.secondOperand = model.currentInput;
-                model.subtraction();
-                model.updateWholeEquation();
-                view.showResult();
-                model.firstOperand = model.tempResult;
-                break;
-            case '*':
-                model.secondOperand = model.currentInput;
-                model.multiplication();
-                model.updateWholeEquation();
-                view.showResult();
-                model.firstOperand = model.tempResult;
-                break;
-            case '/':
-                model.secondOperand = model.currentInput;
-                model.division();
-                model.updateWholeEquation();
-                view.showResult();
-                model.firstOperand = model.tempResult;
-                break;
-        }
-    },
-    triggerOption(value) {
-        let optionTriggered = value;
-        switch (optionTriggered) {
-            case 'AC':
-                controller.allClear();
-                break;
-            case '.':
-                if (isInt(model.currentInput) && !output.textContent.includes(".")) {
-                    model.currentInput += '.';
-                    output.decimals = true;
+                //} 
+                model.performedCalculation = false;
+            } else {
+                console.log()
+                if (digit === '0') {
+                    if (!model.currentInput.length) {
+                        model.currentInput = digit;
+                        view.updateDisplay();
+                    }
+                } else {
+                    model.currentInput == "" ? model.currentInput = digit : model.currentInput += digit;
                     view.updateDisplay();
-                }
 
-                break;
+                };
 
+
+            }
+        },
+        triggerOperator: function (operator) {
+            if (!model.performedCalculation) {
+                controller.performCalculation();
+            }
+            model.performedCalculation = false;
+            model.operator = operator;
+            model.currentInput = output.textContent;
+            model.firstOperand = model.currentInput;
+            model.currentInput = '';
+        },
+        performCalculation: function () {
+            console.log("model exec")
+            model.performedCalculation = true;
+            model.decimals = false;
+
+            if (model.currentInput != "" && model.firstOperand) {
+                model.lastInput = model.currentInput
+                model.currentInput = "";
+            }
+
+            switch (model.operator) {
+                case '+':
+                    model.secondOperand = model.lastInput;
+                    model.addition();
+                    controller.updateWholeEquation();
+                    view.showResult();
+                    model.firstOperand = model.result;
+                    break;
+                case '-':
+                    model.secondOperand = model.lastInput;
+                    model.subtraction();
+                    controller.updateWholeEquation();
+                    view.showResult();
+                    model.firstOperand = model.calcuationResult;
+                    break;
+                case '*':
+                    model.secondOperand = model.lastInput;
+                    model.multiplication();
+                    controller.updateWholeEquation();
+                    view.showResult();
+                    model.firstOperand = model.result;
+                    break;
+                case '/':
+                    model.secondOperand = model.lastInput;
+                    model.division();
+                    controller.updateWholeEquation();
+                    view.showResult();
+                    model.firstOperand = model.result;
+                    break;
+            }
+        },
+        save: function () {
+
+            // Same as below, but in arrow func
+            //var csvContentArrow = "data:text/csv;charset=utf-8," + rows.map(e=>e.join(",")).join("\n");
+
+            var csvContent = "data:text/csv;charset=utf-8,";
+            var wholeEquation = model.wholeEquation;
+            wholeEquation.forEach(function (wholeEquation) {
+                var wholeEquationRow = wholeEquation.join(",");
+                console.log(wholeEquationRow)
+                csvContent += wholeEquationRow + "\r\n";
+            });
+
+            var encodedUri = encodeURI(csvContent);
+            var link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "calculations.csv");
+            document.body.appendChild(link); // Required for FF
+
+            link.click(); // This will download the data file named "my_data.csv".
+            /*fetch("create_csv.php", {
+                method: "POST",
+                mode: "same-origin",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "payload": model.wholeEquation
+                })
+            });*/
+        },
+        triggerOption: function (optionTriggered) {
+
+            switch (optionTriggered) {
+                case 'AC':
+                    controller.allClear();
+                    break;
+                case '.':
+
+                    if (model.currentInput == '') {
+                        model.currentInput = '0.';
+                    } else if (model.currentInput.indexOf('.') == -1) {
+                        model.currentInput += '.';
+                    }
+
+                    model.decimals = true;
+                    view.updateDisplay();
+
+                    break;
+
+            }
+        },
+        allClear: function () {
+            model.performedCalculation = false;
+            model.result = 0;
+            model.firstOperand = 0;
+            model.secondOperand = 0;
+            model.lastInput = 0;
+            model.currentInput = '';
+            model.operator = '';
+            model.decimals = false;
+            view.updateDisplay(reset = true);
+            controller.updateWholeEquation(reset = true);
         }
-    },
-    allClear() {
-        model.executed = false;
-        model.tempResult = 0;
-        model.firstOperand = 0;
-        model.secondOperand = 0;
-        model.currentInput = 0;
-        model.operator = '';
-        model.decimals = false;
-        view.updateDisplay();
-        console.log("allclear");
     }
-}
 
-controller.setUpHandlers();
+    return {
+        model: model,
+        view: view,
+        controller: controller
+    }
 
+}(); // call the WidgetCalculator.controller.setUpHandlers()
 
+WidgetCalculator.controller.setUpHandlers();
 
-function isInt(value) {
-    return !isNaN(value) &&
-        parseInt(Number(value)) == value &&
-        !isNaN(parseInt(value, 10));
-}
-
-
+/*
 // Check if a support for Class exists
 function checkEsSupport() {
     if (typeof Symbol == "undefined") return console.log("aaaaaaaaaaaaaa");
@@ -272,3 +370,5 @@ window.onload = () => {
         console.log(event);
     });
 };
+
+*/
